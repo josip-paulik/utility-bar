73e242f020153efbb12761fd18482f69206e7847
josip_paulik <josip.paulik@lemax.hr>
2020-09-18 11:34:43 +0200

    Create service method which retrieve and add reservations to intenral arra. Also component to show results,.

--- src/app/reservation-managment.service.ts
+++ src/app/reservation-managment.service.ts
@@ -9,21 +9,45 @@ export class ReservationManagmentService {
   reservations: Array<Reservation>;
   units: Array<Unit>;
 
-  constructor() { }
+  startupUnits: Array<Unit> = [
+    new Unit(1, "Paradise", 100),
+    new Unit(2, "Bus: Country roads", 200),
+    new Unit(3, "Hotel California", 999),
+    new Unit(4, "Bus: highway to hell", 500),
+    new Unit(5, "Trip to Wadiya", 450)
+  ]
+
+  startupReservations: Array<Reservation> = [
+    new Reservation(1, this.startupUnits.slice(0, 2)),
+    new Reservation(2, this.startupUnits.slice(3, 6)),
+    new Reservation(3, this.startupUnits.slice(2, 6))
+  ];
+
+  constructor() {
+    this.reservations = this.startupReservations;
+    this.units = this.startupUnits;
+  }
 
   getAllReservations() {
     return this.reservations;
   }
 
   getReservation(id: number) {
-    // return reservation with provided id
+    var reservation = this.reservations.find(x => x.id == id);
+
+    return reservation;
   }
 
-  deleteReservation(id: number) {
-    // delete reservation with provided id
+  addReservation(unitIds: Array<number>) {
+    var unitsToReserve = this.units.filter(x => unitIds.find(y => y == x.id) !== undefined);
+    
+    var newId = this.reservations.length + 1;
+    var reservation = new Reservation(newId, unitsToReserve);
+    
+    this.reservations.push(reservation);
   }
 
   addUnitToReservation(reservationId: number, unitId: number) {
-    // add unit to reservation
+    
   }
 }
--- src/app/reservation.ts
+++ src/app/reservation.ts
@@ -3,4 +3,9 @@ import { Unit } from './unit';
 export class Reservation {
     id: number;
     unitsReserved: Array<Unit>;
+
+    constructor(id: number, unitsReserved: Array<Unit>) {
+        this.id = id;
+        this.unitsReserved = unitsReserved;
+    }
 }
